6c80d62751e8ff4bb2b8df08e7fc84c8e1e65a1f
sorza <alexandre.sorza@outlook.com>
2025-08-02 17:11:31 -0300

    Testando criacao da entidade Library

---
+++ FIAP.PLAY.Tests/Domain/Biblioteca/LibraryTests.cs
@@ -0,0 +1,33 @@
+﻿using FIAP.PLAY.Domain.Library.Entities;
+
+namespace FIAP.PLAY.Tests.Domain.Biblioteca
+{
+    public class LibraryTests
+    {
+        [Fact]
+        public void CriarBiblioteca_DeveCriarBibliotecaValida()
+        {
+            // Arrange
+            long userId = 1;
+
+            // Act
+            var library = Library.Create(userId);
+
+            // Assert
+            Assert.NotNull(library);
+            Assert.Equal(userId, library.UserId);
+            Assert.Empty(library.Games);
+        }
+
+        [Fact]
+        public void CriarBiblioteca_ComUsuarioInvalido_DeveLancarExcecao()
+        {
+            // Arrange
+            long userId = 0; 
+
+            // Act & Assert
+            var exception = Assert.Throws<ArgumentException>(() => Library.Create(userId));
+            Assert.Equal("Usuário inválido. (Parameter 'userId')", exception.Message);
+        }
+    }
+}
