efbfc8f597191c5075683b41b6529ba0821643fe
Jacqueline Albuquerque <jaque200@gmail.com>
2025-08-06 17:34:41 -0300

    Inclusão das entidades Campanha e Promoção.

---
+++ FIAP.PLAY.Domain/Promotions/Entities/Campaign.cs
@@ -0,0 +1,46 @@
+﻿using FIAP.PLAY.Domain.Library.Enums;
+using FIAP.PLAY.Domain.Shared.Entities;
+
+namespace FIAP.PLAY.Domain.Library.Entities
+{
+    public class Campaign : EntityBase
+    {
+
+        #region Properties   
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool IsActive { get; set; }
+
+        #endregion
+
+        #region Construtor
+
+        private Campaign() { }
+
+        private Campaign(string description, DateTime startDate, DateTime endDate, bool isActive)
+        {
+          
+            Description = description;
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+
+        public static Campaign Create(long campaignId, string description, DateTime startDate, DateTime endDate, bool isActive)
+        {
+            if (string.IsNullOrWhiteSpace(description))
+                throw new ArgumentException("Descrição não pode ser vazio.", nameof(description));
+            if (description.Length < 3 || description.Length > 100)
+                throw new ArgumentException("Descrição deve ter entre 3 e 100 caracteres.", nameof(description));
+            if (startDate < DateTime.Today.Date)
+                throw new ArgumentException("Data de início não pode ser menor que hoje.", nameof(startDate));
+            if (endDate < startDate)
+                throw new ArgumentException("Data final não pode ser menor que data inicial.", nameof(endDate));
+
+            return new Campaign(description, startDate, endDate, isActive);
+        }
+        #endregion
+
+    }
+}
+
---
+++ FIAP.PLAY.Infrastructure/EntityConfig/CampaignConfig.cs
@@ -0,0 +1,34 @@
+﻿using FIAP.PLAY.Domain.Library.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FIAP.PLAY.Infrastructure.EntityConfig
+{
+    public class CampaignConfig : IEntityTypeConfiguration<Campaign>
+    {
+        public void Configure(EntityTypeBuilder<Campaign> builder)
+        {
+            builder.ToTable("Campaign");
+
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Id)
+                .ValueGeneratedOnAdd();
+
+            builder.Property(c => c.Description)
+                .HasMaxLength(500);
+
+            builder.Property(c => c.StartDate)
+                .IsRequired();
+
+            builder.Property(c => c.EndDate)
+                .IsRequired();
+
+            builder.Property(c => c.IsActive)
+                .IsRequired()
+                .HasDefaultValue(false);
+
+            builder.HasQueryFilter(c => !c.DateDeleted.HasValue);
+
+        }
+    }
+}
