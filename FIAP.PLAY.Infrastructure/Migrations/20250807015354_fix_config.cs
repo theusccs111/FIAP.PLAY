ee54c2f399ecae7706cb0f4642a3c5d60663ec67
Matheus Chaves Comin Santana <matheus.santana@itajai.quay.com.br>
2025-08-06 22:54:45 -0300

    fix promotion context

---
+++ FIAP.PLAY.Infrastructure/Migrations/20250807015354_fix_config.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FIAP.PLAY.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class fix_config : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_GameLibrary_Game_GameId",
+                table: "GameLibrary");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GameLibrary_Game_GameId",
+                table: "GameLibrary",
+                column: "GameId",
+                principalTable: "Game",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_GameLibrary_Game_GameId",
+                table: "GameLibrary");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GameLibrary_Game_GameId",
+                table: "GameLibrary",
+                column: "GameId",
+                principalTable: "Game",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
